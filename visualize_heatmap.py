5e9869590ced4d1dfa047e5efcf4f0002b5b395f
loidon <lois.dona@mpi.nl>
2024-02-20 13:54:27 +0100

    dummy plotting code aus

--- visualize_heatmap.py
+++ visualize_heatmap.py
@@ -1,21 +1,23 @@
-from feat.plotting import plot_face
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-present_in_pyfeat = ["AU01", "AU02", "AU04", "AU05", "AU06", "AU07", "AU09", "AU11", "AU12", "AU14", "AU15", "AU17", "AU20", "AU23", "AU24", "AU25", "AU26", "AU28", "AU43"]
-
-def compute_mean_activation(data, participant, turn, present_in_pyfeat=present_in_pyfeat):
+def compute_mean_activation(data, participant, turn):
     df_au_activation = pd.read_csv(data)
-    df_participant_turn = df_au_activation.loc["participant_id" == participant & "turn_id" == turn]
-    df_mean_activation = df_participant_turn.mean(axis=0)
+    df_participant_turn = df_au_activation.loc[(df_au_activation["participant_id"] == participant) & (df_au_activation["turn"] == turn)]
+    df_mean_activation = df_participant_turn.iloc[:,4:].mean(axis=0)
+    df_std_activation = df_participant_turn.iloc[:,4:].std(axis=0)
 
-    activations_faceplot = []
-    for au in present_in_pyfeat:
-        activations_faceplot.append(df_mean_activation[au])
-
-    return df_mean_activation, activations_faceplot
+    return df_mean_activation, df_std_activation
 
 if __name__ == "__main__":
-    all_activations, activations_pyfeat = compute_mean_activation("predictions_AU.csv")
-    print(all_activations)
+    mean_activations, std_activations = compute_mean_activation("predictions_AU.csv", "m", "n")
     
+    f = plt.figure()
+    f.set_figwidth(50)
+    plt.rcParams.update({'font.size': 6})
+    plt.bar(list(mean_activations.index), mean_activations)
+    plt.ylabel("Intensity")
+    plt.xlabel("Action unit")
+    plt.ylim(0, 100)
+    plt.show()
